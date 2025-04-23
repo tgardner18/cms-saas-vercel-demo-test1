b6d21797b1e588d63a3eeb4d35e8ddb867a7fee3
tgardner18 <109979067+tgardner18@users.noreply.github.com>
2025-04-23 14:20:27 -0400

    Logo swap

--- apps/frontend/src/components/layout/header/partials/_logo.tsx
+++ apps/frontend/src/components/layout/header/partials/_logo.tsx
@@ -18,11 +18,13 @@ export const Logo: FunctionComponent<LogoProps> = ({
 
   // Get the logo configuration
   const { logo: logoUrl } = useFlag("layout_configuration", defaultValue);
+  const myLogoUrl="https://app-ocstyouversd32tmt001.cms.optimizely.com/globalassets/brand/logo-black-tenerity.png";
+
   return (
     <Link href="/" className="flex items-center grow-0 shrink-0" {...divProps}>
       <Image
-        src={logoUrl}
-        alt="Mosey Bank Logo"
+        src={myLogoUrl}
+        alt="Tenerity Logo"
         fill
         unoptimized
         priority
