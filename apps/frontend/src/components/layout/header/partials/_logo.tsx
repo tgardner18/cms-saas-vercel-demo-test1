4bc36ab1b4d569b6551dc30d41b40fd39cca4496
tgardner18 <109979067+tgardner18@users.noreply.github.com>
2025-05-12 14:49:11 -0400

    Sammons logo update

--- apps/frontend/src/components/layout/header/partials/_logo.tsx
+++ apps/frontend/src/components/layout/header/partials/_logo.tsx
@@ -18,11 +18,13 @@ export const Logo: FunctionComponent<LogoProps> = ({
 
   // Get the logo configuration
   const { logo: logoUrl } = useFlag("layout_configuration", defaultValue);
+  const myLogoUrl="https://app-ocstbloombe71t9cp001.cms.optimizely.com/globalassets/brand/sammonsfinanciallogo.png";
+
   return (
     <Link href="/" className="flex items-center grow-0 shrink-0" {...divProps}>
       <Image
-        src={logoUrl}
-        alt="Mosey Bank Logo"
+        src={myLogoUrl}
+        alt="Sammons Financial Group Logo"
         fill
         unoptimized
         priority
