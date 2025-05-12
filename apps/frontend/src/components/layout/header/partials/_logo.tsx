43fabcf2e87868171e5349ac90bbc85f1590a83d
tgardner18 <109979067+tgardner18@users.noreply.github.com>
2025-05-12 13:56:28 -0400

    Logo try 2

--- apps/frontend/src/components/layout/header/partials/_logo.tsx
+++ apps/frontend/src/components/layout/header/partials/_logo.tsx
@@ -18,7 +18,7 @@ export const Logo: FunctionComponent<LogoProps> = ({
 
   // Get the logo configuration
   //const { logo: logoUrl } = useFlag("layout_configuration", defaultValue);
-  const myLogoUrl="https://app-ocstbloombe71t9cp001.cms.optimizely.com/globalassets/bloom-logo.png";
+  const myLogoUrl="https://app-ocstbloombe71t9cp001.cms.optimizely.com/globalassets/bloom.png";
 
   return (
     <Link href="/" className="flex items-center grow-0 shrink-0" {...divProps}>
