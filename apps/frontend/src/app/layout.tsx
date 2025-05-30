22c9142280f5fd6a173c1442c462616ec5a04ba2
tgardner18 <109979067+tgardner18@users.noreply.github.com>
2025-05-30 10:20:20 -0400

    trying to update the custom google font

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import 'server-only'
 import type { Metadata } from "next";
 import { Figtree } from "next/font/google";
+// custom google font
+import { Anton } from "next/font/google";
 import "./globals.scss";
 import { Body, ThemeProvider } from "@/components/theme"
 import Header from "@/components/layout/header";
@@ -20,6 +22,8 @@ import { SpeedInsights } from "@vercel/speed-insights/next"
 /* eslint-disable @next/next/no-css-tags */
 
 const figtree = Figtree({ subsets: ["latin"] });
+// variable for custom google font
+const anton = Anton({ subsets: ["latin"] }); 
 
 export async function generateMetadata(): Promise<Metadata> {
   const domain = process.env.NEXT_PUBLIC_SITE_DOMAIN || process.env.VERCEL_PROJECT_PRODUCTION_URL || 'localhost:3000'
@@ -84,7 +88,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
         { enableDemoTools && <link key="dynamic-styles" rel="stylesheet" href="/main.css" ></link> }
       </head>
       <ThemeProvider value={{ theme: "system" }}>
-        <Body className={`${figtree.className} on-ghost-white overflow-x-hidden`}>
+        <Body className={`${anton.className} on-ghost-white overflow-x-hidden`}>
           <OptimizelyOneProvider value={{ debug: false }} >
             <PageActivator />
             <div className="flex min-h-screen flex-col justify-between">
